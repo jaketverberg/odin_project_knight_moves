d28ffbe0e09b25d3bcffcbade7b32c24b6522956
Leonerdos <54115094+jaketverberg@users.noreply.github.com>
2022-03-08 16:23:19 -0600

    added #char to print knight when it matches coordinates

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -6,12 +6,15 @@ end
 class Game_board
 
   def draw_board(n = 8)
-    puts "\e[4m                                "
-    until n == 0
-      n -= 1
-      puts "|\e[4m #{} | #{} | #{} | #{} | #{} | #{} | #{} | #{} |"
-    end
+    puts "  \e[4m                         \e[0m"
+      until n == 0
+        n -= 1
+        puts "#{n} \e[4m| #{char(n, 0)} | #{char(n, 1)} | #{char(n, 2)} | #{char(n, 3)} | #{char(n, 4)} | #{char(n, 5)} | #{char(n, 6)} | #{char(n, 7)} |\e[0m"
+      end
+      puts "   0  1  2  3  4  5  6  7"
   end
 
-
+  def char(coordinate, coordinate2)
+    print "♞" if knight.placement == [coordinate, coordinate2]
+  end
 end
